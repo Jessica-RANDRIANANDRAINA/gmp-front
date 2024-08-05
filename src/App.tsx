b23b1bd42125fea7420668380bd9c86516b4c460
silverMichael <emeraudesnaake@yahoo.com>
2024-08-05 12:51:27 +0300

    feat: login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { Route, Routes, useLocation } from "react-router-dom";
+import PageTitle from "./components/PageTitle";
+import Login from "./Pages/Login";
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Routes>
+        <Route
+          index
+          element={
+            <>
+              <PageTitle title="Login" />
+              <Login />
+            </>
+          }
+        ></Route>
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
