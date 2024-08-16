137ad1e6357264991fad47cc68e694b5b70a72db
silverMichael <emeraudesnaake@yahoo.com>
2024-08-16 15:23:05 +0300

    ct: add title and arrow left right

--- src/components/Tables/TableUser.tsx
+++ src/components/Tables/TableUser.tsx
@@ -76,6 +76,64 @@ const TableUser = ({ data }: { data: Array<any> }) => {
           </div>
         </div>
       </div>
+      {/* =====PAGINATE AND TITLE START===== */}
+      <div className=" pb-4 flex justify-between px-3 ">
+        <div className="rotate-180">
+          <svg
+            width="40"
+            height="40"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              {" "}
+              <path
+                d="M6 12H18M18 12L13 7M18 12L13 17"
+                stroke="#000000"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>{" "}
+            </g>
+          </svg> 
+        </div>
+        <div className="text-2xl  font-medium">Listes de tous les utilisateurs</div>
+        <div >
+          
+          <svg
+            width="40"
+            height="40"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              {" "}
+              <path
+                d="M6 12H18M18 12L13 7M18 12L13 17"
+                stroke="#000000"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>{" "}
+            </g>
+          </svg>
+        </div>
+      </div>
+      {/* =====PAGINATE AND TITLE END===== */}
       {/* =====TABLE START===== */}
       <div className="max-w-full overflow-x-auto">
         <table className="w-full table-auto">
