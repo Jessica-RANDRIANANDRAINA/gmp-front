fe284b7a67547046f9273be731e72e8454c0cb21
silverMichael <emeraudesnaake@yahoo.com>
2024-08-16 14:55:25 +0300

    ct(tableuser): selected all

--- src/components/Tables/TableUser.tsx
+++ src/components/Tables/TableUser.tsx
@@ -4,6 +4,12 @@ import { CustomInput, CustomSelect } from "../UIElements";
 
 const TableUser = ({ data }: { data: Array<any> }) => {
   const [userModif, setUserModif] = useState(false);
+  const [dataSorted, setDataSorted] = useState({
+    name: true,
+    email: true,
+    department: true,
+  });
+  const [isAllSelected, setIsAllSelected] = useState(false);
 
   const showData = () => {
     console.log(data);
@@ -76,16 +82,119 @@ const TableUser = ({ data }: { data: Array<any> }) => {
           <thead className="pt-5 rounded-t-xl bg-primaryGreen">
             <tr className=" border border-stone-300 border-opacity-[0.1] border-r-0 border-l-0 text-white text-left">
               <th className=" pl-2 ">
-                <p className="cursor-pointer border w-5 h-5"></p>
+                <button
+                  onClick={() => setIsAllSelected(!isAllSelected)}
+                  className="cursor-pointer border w-5 h-5"
+                >
+                  <svg
+                    width="18"
+                    height="17"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className={`${isAllSelected ? "visible" : "invisible"}`}
+                  >
+                    <path
+                      d="M4 12.6111L8.92308 17.5L20 6.5"
+                      stroke="#fff"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>{" "}
+                  </svg>
+                </button>
               </th>
-              <th className="py-4 px-4 font-bold  text-white dark:text-white xl:pl-11">
+              <th className="py-4 px-4   font-bold  text-white dark:text-white xl:pl-11">
+                <button
+                  onClick={() => {
+                    setDataSorted({
+                      ...dataSorted,
+                      name: !dataSorted.name,
+                    });
+                  }}
+                  className={`${
+                    dataSorted.name ? "" : "rotate-180"
+                  } transform transition-transform duration-200   `}
+                >
+                  <svg
+                    className="fill-current"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
+                      fill=""
+                    ></path>
+                  </svg>
+                </button>
                 Nom
               </th>
-              <th className="py-4 px-4 font-bold  text-white dark:text-white xl:pl-11">
+              <th className="py-4 px-4 font-bold    text-white dark:text-white xl:pl-11">
+                <button
+                  onClick={() => {
+                    setDataSorted({
+                      ...dataSorted,
+                      email: !dataSorted.email,
+                    });
+                  }}
+                  className={`${
+                    dataSorted.email ? "" : "rotate-180"
+                  } transform transition-transform duration-200  `}
+                >
+                  <svg
+                    className="fill-current"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g opacity="0.8">
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
+                        fill=""
+                      ></path>
+                    </g>
+                  </svg>
+                </button>
                 Email
               </th>
-              <th className="py-4 px-4 font-bold  text-white dark:text-white xl:pl-11">
-                Département
+              <th className="py-4 px-4 font-bold flex text-white dark:text-white xl:pl-11">
+                <button
+                  onClick={() => {
+                    setDataSorted({
+                      ...dataSorted,
+                      department: !dataSorted.department,
+                    });
+                  }}
+                  className={`${
+                    dataSorted.department ? "" : "rotate-180"
+                  } transform transition-transform duration-200  `}
+                >
+                  <svg
+                    className="fill-current"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g opacity="0.8">
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
+                        fill=""
+                      ></path>
+                    </g>
+                  </svg>
+                </button>
+                <span>Département</span>
               </th>
               <th className="py-4 px-4 font-bold  text-white dark:text-white xl:pl-11">
                 Accès
@@ -98,7 +207,29 @@ const TableUser = ({ data }: { data: Array<any> }) => {
           <tbody>
             <tr className="hover:bg-whiten dark:hover:bg-whitenGreen">
               <td className="pl-2 ">
-                <p className="cursor-pointer border w-5 h-5"></p>
+                <button
+                  className="cursor-pointer border w-5 h-5"
+                  onClick={() => {
+                    console.log("clicked");
+                  }}
+                >
+                  <svg
+                    width="18"
+                    height="17"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className={`${isAllSelected ? "visible" : "invisible"}`}
+                  >
+                    <path
+                      d="M4 12.6111L8.92308 17.5L20 6.5"
+                      stroke="#000"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>{" "}
+                  </svg>
+                </button>
               </td>
               <td className="border-b border-[#eee] py-5 px-4 pl-9 dark:border-strokedark xl:pl-11">
                 <p className="text-black dark:text-white">
