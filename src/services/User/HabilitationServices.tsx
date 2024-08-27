9a339510bcc915a941dc8bdb97dc661a5e8fe63b
silverMichael <emeraudesnaake@yahoo.com>
2024-08-27 14:08:23 +0300

    update: delete habilitation to delete many

--- src/services/User/HabilitationServices.tsx
+++ src/services/User/HabilitationServices.tsx
@@ -3,7 +3,7 @@ import { HabilitationDataProps } from "../../types/Habilitation";
 
 const endPoint = import.meta.env.VITE_API_ENDPOINT;
 
-// GET
+/* ========= GET ========= */
 // GET ALL HABILITATION
 export const getAllHabilitation = async () => {
   try {
@@ -15,16 +15,16 @@ export const getAllHabilitation = async () => {
 };
 
 // get all habilitation labels
-export const getAllHabilitationLabels = async()=> {
+export const getAllHabilitationLabels = async () => {
   try {
-    const response = await axios.get(`${endPoint}/api/Habilitation/labels`)
-    return response.data
+    const response = await axios.get(`${endPoint}/api/Habilitation/labels`);
+    return response.data;
   } catch (error) {
     throw new Error(`Error at service fetching habilitation labels`);
   }
-}
+};
 
-// POST
+/* ========= POST ========= */
 // CREATE HABILITATION
 export const createHabilitation = async (
   habilitationdata: HabilitationDataProps
@@ -37,5 +37,25 @@ export const createHabilitation = async (
     console.log(`Hablilitation created: ${response.data}`);
   } catch (error) {
     console.error(`Error service creating habilitation: ${error}`);
+  }
+};
+
+// DELETE HABILITATION
+export const deleteHabilitation = async (ids: string[]) => {
+  try {
+    const response = await axios.delete(
+      `${endPoint}/api/Habilitation/deletes`,
+      {
+        data: ids,
+      }
+    );
+
+    if (response.status === 200) {
+      console.log(`Habilitations was been deleted successfully`);
+    } else {
+      console.log(`Failed to delete the habilitation`);
+    }
+  } catch (error) {
+    console.error(`An error occured while deleting the habilitation: ${error}`);
   }
 };
