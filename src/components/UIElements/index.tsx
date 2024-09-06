cf71380ff49ad0c0d350ed890d0b9abdad7c5449
silverMichael <emeraudesnaake@yahoo.com>
2024-09-06 14:42:48 +0300

    feat: make custom input search

--- src/components/UIElements/index.tsx
+++ src/components/UIElements/index.tsx
@@ -2,10 +2,12 @@ import CustomInput from "./Input/CustomInput";
 import CustomSelect from "./Select/CustomSelect";
 import MultiSelect from "./Select/MultiSelect";
 import Checkbox from "./Select/CheckBox";
+import CutomInputUserSearch from "./Input/CutomInputUserSearch";
 
 export {
-    CustomInput, 
-    CustomSelect, 
-    MultiSelect, 
-    Checkbox
-}
+  CustomInput,
+  CutomInputUserSearch,
+  CustomSelect,
+  MultiSelect,
+  Checkbox,
+};
