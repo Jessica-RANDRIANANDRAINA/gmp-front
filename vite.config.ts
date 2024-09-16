ff1978297a27d93ceee4b4935a98fec0d63ef716
silverMichael <emeraudesnaake@yahoo.com>
2024-09-16 07:54:26 +0300

    up: config vite port to 5100 and host all

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5100
+    port: 5100,
+    host: true,
   },
-  preview: {
-    cors: true,
-    host: '0.0.0.0',
-    port: 3000
-  }
 });
-
