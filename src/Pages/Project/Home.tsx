d19516e90807877d7f4284b18d1449f97509b5a2
silverMichael <emeraudesnaake@yahoo.com>
2024-10-03 14:45:03 +0300

    up: welcome page

--- src/Pages/Project/Home.tsx
+++ src/Pages/Project/Home.tsx
@@ -3,7 +3,12 @@ import ProjectLayout from "../../layout/ProjectLayout";
 const Home = () => {
   return (
     <ProjectLayout>
-      <div>Bienvenue dans le projet</div>
+      <div className="grid place-items-center h-[50vh] font-bold text-2xl">
+        <div className="flex flex-col justify-center items-center">
+          <span>Bienvenue dans le G.M.P</span>
+          <span className="text-base font-medium">Gestion et management de projet</span>
+        </div>
+      </div>
     </ProjectLayout>
   );
 };
