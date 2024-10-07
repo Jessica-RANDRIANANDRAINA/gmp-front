4f6977478c8d7870bb2222c749e123440e042ea5
silverMichael <emeraudesnaake@yahoo.com>
2024-10-07 13:01:18 +0300

    up: content of hebdo

--- src/Pages/Project/Hebdo.tsx
+++ src/Pages/Project/Hebdo.tsx
@@ -3,7 +3,11 @@ import ProjectLayout from "../../layout/ProjectLayout";
 const Hebdo = () => {
   return (
     <ProjectLayout>
-      <div>Hebdo</div>
+      <div className="grid place-items-center h-[50vh] font-bold text-2xl">
+        <div className="flex flex-col justify-center items-center">
+          <span>Cette page est en construction 😊</span>
+        </div>
+      </div>
     </ProjectLayout>
   );
 };
