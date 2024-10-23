a41eaa8bb2943d2491bd584a796afc892575959d
silverMichael <emeraudesnaake@yahoo.com>
2024-10-23 14:46:46 +0300

    up: show priority and dueDate in task card

--- src/Pages/Project/ManageProject/TaskManager/PhaseAdvancement.tsx
+++ src/Pages/Project/ManageProject/TaskManager/PhaseAdvancement.tsx
@@ -10,6 +10,7 @@ import {
   deletetaskProject,
 } from "../../../../services/Project";
 import { IPhase } from "../../../../types/Project";
+import { formatDate } from "../../../../services/Function/DateServices";
 import { Notyf } from "notyf";
 import "notyf/notyf.min.css";
 
@@ -38,6 +39,7 @@ const organizeTaskByStatus = (tasks: any[]) => {
         startDate: task.startDate,
         dueDate: task.dueDate,
         userTasks: task.userTasks,
+        status: task.status,
       },
     };
     const columnKey = Object.keys(columns).find(
@@ -292,80 +294,101 @@ const PhaseAdvancement = () => {
                           draggableId={task.id}
                           index={index}
                         >
-                          {(provided, snapshot) => (
-                            <div
-                              ref={provided.innerRef}
-                              {...provided.draggableProps}
-                              {...provided.dragHandleProps}
-                              className={`p-4 mb-1 relative shadow-2 hover:shadow-md hover:shadow-slate-300 dark:hover:shadow-slate-500 text-xs rounded-md  ${
-                                snapshot.isDragging
-                                  ? "bg-green-50 dark:bg-emerald-100"
-                                  : "bg-white dark:bg-boxdark"
-                              }`}
-                              style={{
-                                ...provided.draggableProps.style,
-                              }}
-                              onClick={() => {
-                                setTaskData(task);
-                                setModalUpdateOpen(true);
-                              }}
-                            >
+                          {(provided, snapshot) => {
+                            const endDate = formatDate(task.content.dueDate);
+                            return (
                               <div
-                                className="absolute top-2 right-1 hover:bg-zinc-100 dark:hover:bg-boxdark2 px-1 h-4 cursor-pointer"
-                                onClick={(e) => {
-                                  handleToogleMenuDelete(task.id, e);
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                                className={`p-4 mb-1 relative shadow-2 hover:shadow-md hover:shadow-slate-300 dark:hover:shadow-slate-500 text-xs rounded-md  ${
+                                  snapshot.isDragging
+                                    ? "bg-green-50 dark:bg-emerald-100"
+                                    : "bg-white dark:bg-boxdark"
+                                }`}
+                                style={{
+                                  ...provided.draggableProps.style,
+                                }}
+                                onClick={() => {
+                                  setTaskData(task);
+                                  setModalUpdateOpen(true);
                                 }}
                               >
-                                <svg
-                                  width="15"
-                                  height="15"
-                                  viewBox="0 0 24 24"
-                                  fill="none"
-                                >
-                                  <g id="SVGRepo_bgCarrier"></g>
-                                  <g
-                                    id="SVGRepo_tracerCarrier"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                  ></g>
-                                  <g id="SVGRepo_iconCarrier">
-                                    <path
-                                      d="M12 13.75C12.9665 13.75 13.75 12.9665 13.75 12C13.75 11.0335 12.9665 10.25 12 10.25C11.0335 10.25 10.25 11.0335 10.25 12C10.25 12.9665 11.0335 13.75 12 13.75Z"
-                                      className="fill-black dark:fill-white"
-                                    ></path>{" "}
-                                    <path
-                                      d="M19 13.75C19.9665 13.75 20.75 12.9665 20.75 12C20.75 11.0335 19.9665 10.25 19 10.25C18.0335 10.25 17.25 11.0335 17.25 12C17.25 12.9665 18.0335 13.75 19 13.75Z"
-                                      className="fill-black dark:fill-white"
-                                    ></path>{" "}
-                                    <path
-                                      d="M5 13.75C5.9665 13.75 6.75 12.9665 6.75 12C6.75 11.0335 5.9665 10.25 5 10.25C4.0335 10.25 3.25 11.0335 3.25 12C3.25 12.9665 4.0335 13.75 5 13.75Z"
-                                      className="fill-black dark:fill-white"
-                                    ></path>{" "}
-                                  </g>
-                                </svg>
-                              </div>
-                              {/* pop up menu delete */}
-                              {activeTaskId === task.id && (
                                 <div
-                                  ref={deletePopUp}
-                                  className="absolute z-20 right-0 top-5 bg-white dark:bg-boxdark dark:border-formStrokedark border-zinc-100 dark:hover:border-red-950 border shadow-lg rounded-md "
+                                  className="absolute top-2 right-1 hover:bg-zinc-100 dark:hover:bg-boxdark2 px-1 h-4 cursor-pointer"
+                                  onClick={(e) => {
+                                    handleToogleMenuDelete(task.id, e);
+                                  }}
                                 >
-                                  <button
-                                    onClick={(e) => {
-                                      e.stopPropagation();
-                                      handleDeleteTask(task.id);
-                                      console.log("delete task id: ", task.id);
-                                    }}
-                                    className="text-red-600 dark:text-red-400 dark:hover:bg-red-950  hover:bg-red-50 px-4 py-2 rounded"
+                                  <svg
+                                    width="15"
+                                    height="15"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
                                   >
-                                    Supprimer
-                                  </button>
+                                    <g id="SVGRepo_bgCarrier"></g>
+                                    <g
+                                      id="SVGRepo_tracerCarrier"
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                    ></g>
+                                    <g id="SVGRepo_iconCarrier">
+                                      <path
+                                        d="M12 13.75C12.9665 13.75 13.75 12.9665 13.75 12C13.75 11.0335 12.9665 10.25 12 10.25C11.0335 10.25 10.25 11.0335 10.25 12C10.25 12.9665 11.0335 13.75 12 13.75Z"
+                                        className="fill-black dark:fill-white"
+                                      ></path>{" "}
+                                      <path
+                                        d="M19 13.75C19.9665 13.75 20.75 12.9665 20.75 12C20.75 11.0335 19.9665 10.25 19 10.25C18.0335 10.25 17.25 11.0335 17.25 12C17.25 12.9665 18.0335 13.75 19 13.75Z"
+                                        className="fill-black dark:fill-white"
+                                      ></path>{" "}
+                                      <path
+                                        d="M5 13.75C5.9665 13.75 6.75 12.9665 6.75 12C6.75 11.0335 5.9665 10.25 5 10.25C4.0335 10.25 3.25 11.0335 3.25 12C3.25 12.9665 4.0335 13.75 5 13.75Z"
+                                        className="fill-black dark:fill-white"
+                                      ></path>{" "}
+                                    </g>
+                                  </svg>
                                 </div>
-                              )}
-                              {/* pop up menu delete */}
-                              {task.content.title}
-                            </div>
-                          )}
+                                {/* pop up menu delete */}
+                                {activeTaskId === task.id && (
+                                  <div
+                                    ref={deletePopUp}
+                                    className="absolute z-20 right-0 top-5 bg-white dark:bg-boxdark dark:border-formStrokedark border-zinc-100 dark:hover:border-red-950 border shadow-lg rounded-md "
+                                  >
+                                    <button
+                                      onClick={(e) => {
+                                        e.stopPropagation();
+                                        handleDeleteTask(task.id);
+                                        console.log(
+                                          "delete task id: ",
+                                          task.id
+                                        );
+                                      }}
+                                      className="text-red-600 dark:text-red-400 dark:hover:bg-red-950  hover:bg-red-50 px-4 py-2 rounded"
+                                    >
+                                      Supprimer
+                                    </button>
+                                  </div>
+                                )}
+                                {/* pop up menu delete */}
+                                <div>{task.content.title}</div>
+                                <div
+                                  className={` pt-1 mt-2 text-xs ${
+                                    task.content.priority === "Moyen" ||
+                                    task.content.priority === "Bas"
+                                      ? "hidden "
+                                      : "text-orange"
+                                  }`}
+                                >
+                                  {task.content.priority}
+                                </div>
+                                <div
+                                  className={`border-t border-t-zinc-200 pt-1 mt-2 text-xs ${endDate==="--" ? "hidden": ""}`}
+                                >
+                                  {endDate}
+                                </div>
+                              </div>
+                            );
+                          }}
                         </Draggable>
                       ))}
                       {provided.placeholder}
