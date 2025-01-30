57e28b8f8d054b3dcf1430898afc4f2f7fb8ea76
silverMichael <emeraudesnaake@yahoo.com>
2025-01-30 13:18:10 +0300

    up: message to be jsx element

--- src/constants/NotificationMessage.tsx
+++ src/constants/NotificationMessage.tsx
@@ -1,18 +1,43 @@
-const getNotificationCreateProject = (
+export const getNotificationCreateProject = (
   projectName: string,
-  role: "director" | "member" | "observator"
-): string => {
+  role: "director" | "member" | "observator",
+  projectid: string
+): JSX.Element => {
   const actions: { [key in "director" | "member" | "observator"]: string } = {
     director: "le gérer",
     member: "voir les détails",
     observator: "suivre son avancement",
   };
 
-  return `Vous avez été ajouté au projet '${projectName}' en tant ${
-    role === "director"
-      ? "que chef de projet"
-      : role === "member"
-      ? "que membre"
-      : "qu'observateur"
-  }.  Cliquez ici pour ${actions[role]}!`;
+  return (
+    <>
+      Vous avez été ajouté au projet <strong>"{projectName}"</strong> en tant{" "}
+      {role === "director" ? (
+        <strong>que Chef de projet</strong>
+      ) : role === "member" ? (
+        <strong>que Membre</strong>
+      ) : (
+        <strong>qu'Observateur</strong>
+      )}
+      .{" "}
+      <a
+        href={` ${
+          role === "director"
+            ? `${
+                import.meta.env.VITE_API_FRONT
+              }/gmp/project/update/${projectid}`
+            : role === "member"
+            ? `${
+                import.meta.env.VITE_API_FRONT
+              }/gmp/project/details/${projectid}/details`
+            : `${
+                import.meta.env.VITE_API_FRONT
+              }/gmp/project/details/${projectid}/details`
+        }  `}
+        className="italic text-xs text-blue-400 hover:text-blue-600"
+      >
+        Cliquez ici pour {actions[role]}!
+      </a>
+    </>
+  );
 };
