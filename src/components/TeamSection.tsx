b1d1b6585dbea34b16470ef895b34ede7f59ce82
silverMichael <emeraudesnaake@yahoo.com>
2025-02-20 11:46:43 +0300

    up(TeamSection): add border, slice name, ellipsis

--- src/components/TeamSection.tsx
+++ src/components/TeamSection.tsx
@@ -13,58 +13,53 @@ const TeamSection: React.FC<{ listUsers?: IUserProject[] }> = ({
     Observateur:
       "bg-amber-100 text-amber-600 border-amber-300  dark:bg-amber-900 dark:text-amber-300 dark:border-amber-700",
   };
+
   const groupedUsers: Record<string, IUserProject[]> = {
-    "Chef de projet":
-      listUsers?.filter((user) => user.role === "director") || [],
-    Membre: listUsers?.filter((user) => user.role === "member") || [],
-    Observateur: listUsers?.filter((user) => user.role === "observator") || [],
+    "Chef de projet": listUsers.filter((user) => user.role === "director"),
+    Membre: listUsers.filter((user) => user.role === "member"),
+    Observateur: listUsers.filter((user) => user.role === "observator"),
   };
+
+  const truncateName = (name: string, maxNameLength = 19) => {
+    const parenthesisIndex = name.lastIndexOf("(");
+
+    const namePart = name.slice(0, parenthesisIndex).trim();
+    const parenthesisPart = name.slice(parenthesisIndex).trim();
+
+    const truncatedName =
+      namePart.length > maxNameLength
+        ? `${namePart.slice(0, maxNameLength)}...`
+        : namePart;
+
+    return `${truncatedName} ${parenthesisPart}`;
+  };
+
   return (
     <div>
-      <div className="text-base">
-        <u>
-          <span className="uppercase">É</span>quipe
-        </u>{" "}
-        :
-      </div>
+      <div className="text-base">Équipe :</div>
 
       <div className="space-y-4">
         {Object.entries(groupedUsers).map(
           ([role, users]) =>
             users.length > 0 && (
               <div key={role}>
-                {/* <h2 className="text-lg font-semibold capitalize mb-2">
-                  {role === "cpd"
-                    ? "Chef de projet"
-                    : role === "member"
-                    ? "Membres"
-                    : "Observateurs"}
-                </h2> */}
-
                 <div className="flex space-y-1 flex-wrap">
                   {users.map((userProject) => (
                     <div
                       key={userProject.user.id}
-                      className="flex items-center mr-5 gap-3 p-2 rounded-lg shadow-sm "
+                      className="flex items-center mr-5 gap-3 p-2 rounded-lg shadow-sm border border-stroke dark:border-strokedark w-62.5 overflow-hidden"
                     >
-                      {/* <img
-                        src={
-                          userProject.user.profilePicture ||
-                          "/default-avatar.png"
-                        }
-                        alt={userProject.user.name}
-                        className="w-12 h-12 rounded-full object-cover border"
-                      /> */}
-                      <div className="border border-stroke dark:border-strokedark w-12 h-12 flex items-center justify-center rounded-full bg-gray-300 text-gray-800 font-bold text-lg">
+                      <div className="border border-stroke p-2 dark:border-strokedark w-8 h-8 flex items-center justify-center rounded-full bg-gray-300 text-gray-800 font-bold ">
                         {getInitials(userProject.user.name)}
                       </div>
 
-                      <div >
-                        <p className="text-sm font-medium">
-                          {userProject.user.name}
+                      <div className="w-full">
+                        <p className="text-xs overflow-hidden whitespace-nowrap ">
+                          {truncateName(userProject.user.name)}
                         </p>
+
                         <span
-                          className={`  text-xs px-2 py-1 rounded-full ${roleColors[role]}`}
+                          className={`text-xs px-2 rounded-full ${roleColors[role]}`}
                         >
                           {role}
                         </span>
